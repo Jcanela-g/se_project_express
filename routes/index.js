d2e7a7de05a7751a48395605f3d23b96edda8522
Jose Canela <jose.canela1693@gmail.com>
2025-06-23 23:05:57 -0400

    swap routes in index.js so GET/items can load properly

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 
-const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
+const userRouter = require("./users");
+
 const { NOT_FOUND, NOT_FOUND_MESSAGE } = require("../utils/errors");
 
-router.use("/", userRouter);
 router.use("/items", itemRouter);
+router.use("/", userRouter);
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: NOT_FOUND_MESSAGE });
 });
